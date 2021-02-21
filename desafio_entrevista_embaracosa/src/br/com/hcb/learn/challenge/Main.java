852f6031945c10222a7fcc55fb2a609ce2796249
Hugaleno Bezerra <hugaleno@gmail.com>
2021-02-21 17:54:27 -0300

    Desafio entrevista embaracosa

---
+++ desafio_entrevista_embaracosa/src/br/com/hcb/learn/challenge/Main.java
@@ -0,0 +1,31 @@
+package br.com.hcb.learn.challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+
+    while ((line = br.readLine()) != null) {
+      boolean isRepeated = false;
+      for (int i = 1; i < line.length(); i++) {
+        String sub1 = line.substring(0, i);
+        String sub2 = line.substring(i);
+        if (sub1.endsWith(sub2)) {
+          System.out.println(sub1);
+          isRepeated = true;
+        }
+      }
+      if (!isRepeated) {
+        System.out.println(line);
+      }
+    }
+  }
+}
